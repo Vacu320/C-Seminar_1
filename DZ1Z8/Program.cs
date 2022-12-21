57e03c60356691898c39ed5bc21322b565e67e94
Vadim <mysh_320@mail.ru>
2022-12-21 20:15:34 +0300

    Домашнее задание 4

---
+++ DZ1Z8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Четвертое домашнее задание. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count = count + 1;
+} 
