55516696abeab12982e7b56695897b8bfec0af5b
Vadim <mysh_320@mail.ru>
2022-12-21 20:15:06 +0300

    Домашнее задание 2

---
+++ DZ1Z4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Второе домашнее задание. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: ");
+
+int numberA = int.Parse(Console.ReadLine());
+
+int numberB = int.Parse(Console.ReadLine());
+
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+System.Console.WriteLine($"максимальное из этих чисел {max}");
+
+
